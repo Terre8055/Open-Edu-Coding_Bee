ae2a6cb91a6a4f51c244d67b8358d528bdffd138
Terre8055 <michaelappiah2018@icloud.com>
2023-03-29 16:51:08 +0000

    Handle response to client side for login endpoint

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -1,5 +1,6 @@
-const dbModel = require('../models/userModels')
-const connectDb = require('../dbConnect/dbConnect')
+const bcrypt = require('bcrypt')
+const User = require('../models/userModels')
+const asyncHandler = require('express-async-handler')
 
 // const bodyParser = require('body-parser')
 
@@ -7,32 +8,71 @@ const connectDb = require('../dbConnect/dbConnect')
 //@route POST /api/users/register
 //access public
 
-const registerUser = async(req, res) => {
-    const {name, password, email} = req.body
+const registerUser = asyncHandler(async(req, res) => {
+    const {firstname, lastname, password, email, phone} = req.body
     //check input to find null values
-    if(!name || !email || !password){
+    if(!firstname || !lastname || !email || !password ||!phone){
         res.status(400);
         throw new Error("All fields are mandatory")
     }
 
     //check to see if user already exist
-    // const userAvailable = await User.findOne({email});
-    // if (userAvailable){
-    //     res.status(400);
-    //     throw new Error("User already registered")
-    // }
 
-   
-    
-    res.json('Account created successfully')
+    const userAvailable = await User.findOne({email});
+    if (userAvailable){
+        res.status(400);
+        throw new Error("User already registered")
+    }
+
+    //hash password
+    const hashedPassword = await bcrypt.hash(password, 10)
+    console.log(`Hash: ${hashedPassword}`)
+
+    //create user
+    const user = await User.create({
+        firstname,
+        lastname,
+        email,
+        password: hashedPassword,
+        phone
+    })
+    console.log(`User created ${user}`)
+
+    //user created successfully?
+    if(user){
+        res.status(201).json('Success')
+    }else{
+        res.status(400)
+        throw new Error("User data is not valid")
+    }
+    res.json({message: 'Register the user'})
 }
-
+)
 //@desc Login User
 //@route POST /api/users/register
 //access public
 
-const loginUser = (req, res) => {
-    res.json('hi')
-}
+const loginUser = asyncHandler(async(req, res) => {
+    const {email, password} = req.body
+
+    //check to find empty fields
+    if(!email || !password){
+        res.status(400); //validation_error
+        throw new Error("All fields are mandatory")
+    }
+
+    //check if user exists in database
+    const user = await User.findOne({email})
+
+    //if credential matches 
+
+    if(user && (await bcrypt.compare(password, user.password))){
+        res.status(201).json('Success')
+    }else{
+        res.status(401)
+        throw new Error("Failed to validate credentials")
+
+    }
+})
 
 module.exports = {registerUser, loginUser}
