d0b1581e3de4271494082eba920d5e0c4cc9417f
Terre8055 <michaelappiah2018@icloud.com>
2023-03-29 13:26:40 +0000

    Establish connectio to mongo server

--- Server/dbConnect/dbConnect.js
+++ Server/dbConnect/dbConnect.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose')
 
+//establish connection to the mongo server
 
 
 const connectDb = async () => {
     try{
         const connect = await mongoose.connect(process.env.CONNECTION_STRING)
-        // connect.connection.name = 'mycontacts-backend'
+        // connect.connection.name = 'mycontacts-backend ---- ignore-----'
         console.log("Database connected: ", connect.connection.host, connect.connection.name, connect.connection.port) 
     }catch(err){
         console.log(err);
