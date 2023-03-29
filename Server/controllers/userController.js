511f698ac470fa131ccc2b6fb3086ea67f2d1fc3
Terre8055 <michaelappiah2018@icloud.com>
2023-03-29 13:21:47 +0000

    Install mongoose package

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -1,3 +1,6 @@
+const dbModel = require('../models/userModels')
+const connectDb = require('../dbConnect/dbConnect')
+
 // const bodyParser = require('body-parser')
 
 //@desc Register User
@@ -7,23 +10,19 @@
 const registerUser = async(req, res) => {
     const {name, password, email} = req.body
     //check input to find null values
-    if(!username || !email || !password){
+    if(!name || !email || !password){
         res.status(400);
         throw new Error("All fields are mandatory")
     }
 
     //check to see if user already exist
-    const userAvailable = await User.findOne({email});
-    if (userAvailable){
-        res.status(400);
-        throw new Error("User already registered")
-    }
+    // const userAvailable = await User.findOne({email});
+    // if (userAvailable){
+    //     res.status(400);
+    //     throw new Error("User already registered")
+    // }
 
-    const createAccount = {
-        name,
-        password,
-        email
-    }
+   
     
     res.json('Account created successfully')
 }
