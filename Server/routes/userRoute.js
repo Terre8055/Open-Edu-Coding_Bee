24c58d027f814b78822a63234075aea9a0489cf4
Terre8055 <michaelappiah2018@icloud.com>
2023-04-14 22:33:49 +0000

    Unpack pug dependency

--- Server/routes/userRoute.js
+++ Server/routes/userRoute.js
@@ -6,4 +6,16 @@ const router = express.Router()
 router.post('/login', loginUser)
 router.post('/register', registerUser)
 
+//Testing redirects and pug render
+// please ignore below
+router.get('/user/info', (req,res) => res.json({message:'hi'}))
+
+router.get('/users/details', (req, res) => {
+    console.log(req.ip)
+    res.render('error', { message: 'Redirecting you to appropriate site' })
+    res.redirect('/auth/register')
+
+    
+})
+
 module.exports = router
